6a0c23ebabce17fe9342aadc4ab9626363a48f1e
Garenge <garenge@outlook.com>
2025-11-01 23:43:27 +0800

    Initial Commit

---
+++ MacTool/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MacTool
+//
+//  Created by Garenge on 2025/11/1.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
